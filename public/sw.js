bcbd932668808b900df66531bb8e7ba8096d301d
jiftechnify <jiftech.stlfy@gmail.com>
2023-10-19 02:15:03 +0900

    delete all  previous SW caches on activation

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,16 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (e) => {
+  const deleteCache = async () => {
+    for (const key of await caches.keys()) {
+      await caches.delete(key);
+    }
+  };
+  e.waitUntil(deleteCache());
+});
+
 self.addEventListener("fetch", () => {
   return;
 });
